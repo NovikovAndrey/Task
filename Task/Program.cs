6a8ac0f21ef22205e05c5c5acd36a65b099b8826
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-11 19:25:27 +0300

    Rebuild

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
 
-namespace Task
+namespace MyTask
 {
     class Program
     {
+        static void Factorial(int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"Факториал равен {result}");
+        }
+
+        // определение асинхронного метода
+        static async Task FactorialAsync(int n)
+        {
+            await Task.Run(() => Factorial(n));
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var t = FactorialAsync(5);
+            var t1 = FactorialAsync(6);
+            Console.WriteLine("Некоторая работа");
+            Console.Read();
         }
     }
 }
